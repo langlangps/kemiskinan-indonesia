0a35b4b74ca0c65d6d4c23351eda591893911c23
langlangps <langlangps0@gmail.com>
2021-05-28 22:09:28 +0700

    Mengubah line chart

--- home/dash_app/finished_app/LineChart.py
+++ home/dash_app/finished_app/LineChart.py
@@ -8,6 +8,7 @@ import pandas as pd
 kedalaman_kemiskinan = pd.read_excel('https://raw.githubusercontent.com/langlangps/datasets/main/Proyek%20Visdat/Indeks_Kedalaman_Kemiskinan_Per_Provinsi_2012_2020.xlsx')
 keparahan_kemiskinan = pd.read_excel('https://raw.githubusercontent.com/langlangps/datasets/main/Proyek%20Visdat/Indeks_Keparahan_Kemiskinan_Per_Provinsi_2012-2020.xlsx')
 presentase_miskin = pd.read_excel('https://raw.githubusercontent.com/langlangps/datasets/main/Proyek%20Visdat/Presentase_Penduduk_Miskin_Per_Provinsi_2012_2020.xlsx')
+# penduduk_miskin = pd.read_excel('https://raw.githubusercontent.com/langlangps/datasets/main/Proyek%20Visdat/Jumlah_Penduduk_Miskin_Per_KabKot_2015_2020.xlsx')
 
 kedalaman_kemiskinan = kedalaman_kemiskinan[kedalaman_kemiskinan.Provinsi != 'INDONESIA']
 keparahan_kemiskinan = keparahan_kemiskinan[keparahan_kemiskinan.Provinsi != 'INDONESIA']
@@ -23,6 +24,13 @@ jawa_bali_nusa = ('DKI Jakarta', 'DI Yogyakarta', 'Banten', 'Jawa Tengah', 'Nusa
 sulawesi = ('Sulawesi Tenggara', 'Gorontalo', 'Sulawesi Barat', 'Sulawesi Selatan', 'Sulawesi Utara', 'Sulawesi Tengah')
 papua_maluku = ('Maluku', 'Papua', 'Maluku Utara', 'Papua Barat')
 kalimantan = ('Kalimantan Selatan', 'Kalimantan Tengah', 'Kalimantan Barat', 'Kalimantan Timur', 'Kalimantan Utara')
+pulau = {
+    'SUMATERA':sumatera, 
+    'JAWA, BALI, DAN NUSA TENGGARA':jawa_bali_nusa, 
+    'KALIMANTAN':kalimantan,
+    'SULAWESI':sulawesi,
+    'MALUKU DAN PAPUA':papua_maluku
+}
 
 def get_pulau(provinsi):
   if provinsi in [a.upper() for a in sumatera]:
@@ -56,12 +64,30 @@ app.layout = html.Div([
         searchable = False,
         value = 'kedalaman_kemiskinan'
     ),
-    dcc.Dropdown(
-        id = 'province-selector',
-        options = [
-            {'label':a, 'value':a} for a in kedalaman_kemiskinan.Provinsi.unique()
-        ],
-        multi = True,
+    html.Div(
+        [dcc.Dropdown(
+            id = 'show-type',
+            options = [
+                {'label':'Pulau', 'value':'by_pulau'},
+                {'label':'Provinsi','value':'by_provinsi'},
+            ],
+            value='by_province'
+        )],
+        style = {'width':'48%', 'display':'inline-block'}
+    ),
+    html.Div(
+        [dcc.Dropdown(
+            id = 'variabel1-show',
+            multi=True
+        )],
+        style = {'width':'48%', 'display':'inline-block'}
+    ),
+    html.Div(
+        [dcc.Dropdown(
+            id = 'variabel2-show',
+            multi=True
+        )],
+        style = {'width':'48%', 'display':'inline-block'}
     ),
     dcc.Slider(
         id = 'year-slider',
@@ -85,7 +111,7 @@ app.layout = html.Div([
     Output('line-chart-kemiskinan-indonesia', 'figure'),
     Input('dropdown-figure-type', 'value'),
     Input('year-slider', 'value'),
-    Input('province-selector','value')
+    Input('show-type','value')
 )
 
 def update_figure(figure_type,year_value,province_selector):
@@ -152,3 +178,38 @@ def update_figure(figure_type,year_value,province_selector):
 
     return fig
 
+@app.callback(
+    Output('variabel1-show', 'options'),
+    Output('variabel2-show', 'options'),
+    Input('show-type', 'value')
+)
+
+def set_variabel1_show(show_type):
+    if show_type == 'by_pulau':
+        return ([{'label':a,'value':a} for a in pulau.keys()], None)
+    if show_type == 'by_province':
+        return ([{'label':a,'value':a} for a in kedalaman_kemiskinan.Provinsi.unique()], None)
+
+@app.callback(
+    Output('variabel2-show', 'options'),
+    Input('variabel1-show', 'value'),
+    Input('show-type', 'value')
+)
+
+def set_variabel2_show(variabel1_show,show_type):
+    if show_type== 'by_province':
+        return None
+    elif show_type == 'by_pulau':
+        return sorted([a for a in pulau[variabel1_show]])
+
+@app.callback(
+    Output('variabel1-show','value'),
+    Output('variabel2-show','value'),
+    Input('show-type','value')
+)
+
+def set_variabel_value(show_type):
+    if show_type == 'by_pulau':
+        return [a for a in pulau.keys()], [a for a in kedalaman_kemiskinan.Provinsi.unique()]
+    elif show_type == 'by_province':
+        return [a for a in kedalaman_kemiskinan.Provinsi.unique()], None
